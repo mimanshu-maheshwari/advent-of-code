a5b08dbd6e6d0e3beb57ab8de990ccb7065ffa34
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-12-03 23:36:03 +0530

    Update part1 of day 2

--- 2024/rust/advent-of-code/day-02/src/part1.rs
+++ 2024/rust/advent-of-code/day-02/src/part1.rs
@@ -1,6 +1,41 @@
 use super::Result;
-pub fn solve(_input: String) -> Result<String> {
-    todo!()
+pub fn solve(input: String) -> Result<String> {
+    let mut result = 0;
+    for line in input.split('\n') {
+        if line.trim().is_empty() {
+            continue;
+        }
+        let values: Vec<_> = line
+            .split_whitespace()
+            .map(|v| v.trim().parse::<isize>().unwrap())
+            .collect();
+
+        if values.is_empty() {
+            continue;
+        } else if values.len() == 1 {
+            result += 1;
+        }
+
+        let increasing = values[0] - values[1] <= 0;
+        let mut is_safe = true;
+        for i in 0..(values.len() - 1) {
+            let dif = (values[i] - values[i + 1]).abs();
+            if increasing {
+                if values[i] >= values[i + 1] || dif > 3 {
+                    is_safe = false;
+                    break;
+                }
+            } else if values[i] <= values[i + 1] || dif > 3 {
+                is_safe = false;
+                break;
+            }
+        }
+
+        if is_safe {
+            result += 1;
+        }
+    }
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -8,8 +43,13 @@ mod tests {
     use super::solve;
     #[test]
     fn example() {
-        let input = "";
-        let expected = "";
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        let expected = "2";
         let actual = solve(input.to_owned()).expect("Failed to solve input.");
         assert_eq!(expected, actual);
     }
